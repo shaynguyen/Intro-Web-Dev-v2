b0c2268c89d3f34a4452fa992afa74dd1e61050b
shay <shay.nguyen@gmail.com>
2020-11-09 14:20:31 -0800

    Refactor doggo.js

--- 03-DOG_PROJECT/public/doggo.js
+++ 03-DOG_PROJECT/public/doggo.js
@@ -1,52 +1,47 @@
 const LIST_ALL_BREEDS_ENDPOINTS = "https://dog.ceo/api/breeds/list/all"
 
+fetchAndPopulateBreedsMenu();
+
 document
   .querySelector(".doggo-button")
   .addEventListener("click", () => fetchDog())
 
-fetchBreedListFromEndpoint();
+/* ******************************************** */
 
-/* ***** */
-
-function fetchBreedListFromEndpoint() {
+function fetchAndPopulateBreedsMenu() {
   fetch(LIST_ALL_BREEDS_ENDPOINTS)
-    .then((response) => {
+    .then(response => {
       return response.json()
     })
-    .then((responseJSON) => {
-      const result = responseJSON.message
-      const listOfBreeds = Object.keys(result)
-      const fullBreedList = []
-      listOfBreeds.forEach(breed => {
-        const subBreeds = []
-        if (result[breed].length) {
-          result[breed].forEach((subBreed) => {
-            subBreeds.push(subBreed)
-          })
-        }
-        fullBreedList.push({breed, subBreeds})
-      })
-      return fullBreedList
-    })
-    .then((list) => {
-      updateSelectMenu(list)
+    .then(responseJSON => {
+      populateSelectMenu(responseJSON)
     })
 }
 
-function updateSelectMenu(list) {
+function populateSelectMenu(responseJSON) {
+  const dogs = responseJSON.message
+  const breeds = Object.keys(dogs)
   const breedSelect = document.querySelector(".breed-select")
 
-  list.forEach((breed) => {
+  breeds.forEach(breed => {
     const opt = document.createElement("option")
-    if (breed.subBreeds.length) {
-      breed.subBreeds.forEach(subBreed => {
-        opt.value = `${breed.breed}/${subBreed}`
-        opt.innerText = `${breed.breed} ${subBreed}`
+
+    if (dogs[breed].length) {
+      dogs[breed].forEach(subBreed => {
+        /* why the "/" in opt.value ?
+         * to get sub-breed from endpoint we need: .../breed/${breed}/${subBreed}/images/ ...
+         * we'll use opt.value for breed, and if value has a sub-breed, it
+         * will provide an additional sub route for the sub-breed:
+         * ".../australian/shepherd/..." vs .../beagle/...
+         */
+        opt.innerText = `${breed} ${subBreed}`
+        opt.value = `${breed}/${subBreed}`
       })
     } else {
-      opt.value = breed.breed
-      opt.innerText = breed.breed
+      opt.innerText = breed
+      opt.value = opt.innerText
     }
+
     breedSelect.appendChild(opt)
   })
 }
@@ -54,32 +49,34 @@ function updateSelectMenu(list) {
 function fetchDog() {
   const select = document.querySelector(".breed-select")
   const breed = select.options[select.selectedIndex].value
-  const doggoPromise = fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+  displayLoadingGif();
 
-  let img = document.querySelector(".doggo-img")
-  if (!img) {
-    img = document.createElement("img")
-    img.alt= "loading gif"
-  }
-  img.src = "./public/loading.gif"
-
-  doggoPromise
-    .then((response) => {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => {
       return response.json()
     })
-    .then((resultJSON) => {
+    .then(resultJSON => {
       displayDog(resultJSON)
     })
 }
 
+function displayLoadingGif() {
+  let img = document.querySelector(".doggo-img")
+  if (!img) {
+    img = document.createElement("img")
+  }
+  img.alt = "loading gif"
+  img.src = "./public/loading.gif"
+}
 
 function displayDog(resultJSON) {
-  let doggo = document.querySelector(".doggo-img")
-  if (!doggo) {
-    doggo = document.createElement("img")
-    doggo.className = "doggo-img"
-    doggo.alt = "a doggo"
-    document.querySelector(".doggo").appendChild(doggo)
+  let img = document.querySelector(".doggo-img")
+  let dogImgSrc = resultJSON.message
+  if (!img) {
+    img = document.createElement("img")
+    document.querySelector(".doggo").appendChild(img)
+    img.className = "doggo-img"
   }
-  doggo.src = resultJSON.message
+  img.alt = "a doggo"
+  img.src = dogImgSrc
 }
